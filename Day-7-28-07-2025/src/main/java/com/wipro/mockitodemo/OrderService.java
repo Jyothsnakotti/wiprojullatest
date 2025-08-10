eaee9cddf9209fd27ba9f0ff24be1f165316162f
Jyothsnakotti <127415199+Jyothsnakotti@users.noreply.github.com>
2025-08-10 16:37:38 +0530

    Delete Day-7-28-07-2025 directory

--- Day-7-28-07-2025/src/main/java/com/wipro/mockitodemo/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.mockitodemo;
-
-public class OrderService {
-
-	public String placeOrder(String orderItem) {
-        // default implementation returns null
-        return null;
-    }
-}
