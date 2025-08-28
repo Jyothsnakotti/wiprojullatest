7938fa7541485bb18e3157f92c9be2b8a28d2b1b
Jyothsnakotti <127415199+Jyothsnakotti@users.noreply.github.com>
2025-08-28 16:03:24 +0530

    Delete 19-08-2025 directory

--- 19-08-2025/thirdapp/src/app/models/fruit.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IFruit {
-  name: string;
-  description: string;
-  image: string;
-  price: number;
-}
--- 19-08-2025/thirdapp/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/Ex-8/app';
-
-bootstrapApplication(App);
